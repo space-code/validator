44555a93c4ea35233184b6eea8b5169ca8dce797
Nikita Vasilev <nv3212@gmail.com>
2023-09-23 19:52:14 +0400

    Implenent a validation extension for `SwiftUI` views

---
+++ Sources/ValidatorUI/Classes/SUI/Extensions/View+Validation.swift
@@ -0,0 +1,38 @@
+//
+// Validator
+// Copyright © 2023 Space Code. All rights reserved.
+//
+
+// swiftlint:disable prefixed_toplevel_constant
+
+import SwiftUI
+import ValidatorCore
+
+private let validator = Validator()
+
+public extension View {
+    /// Validate a binding item using a set of validation rules and perform an action based on
+    /// the validation result.
+    ///
+    /// This function takes a binding item, an array of validation rules, and a closure to handle
+    /// the validation result.
+    /// It validates the wrapped value of the binding item against the specified rules and then
+    /// invokes the provided action with the result.
+    ///
+    /// - Parameters:
+    ///   - item: The binding item to validate.
+    ///   - rules: An array of validation rules to apply to the item's value.
+    ///   - action: A closure that takes a `ValidationResult` as its parameter.
+    ///             This closure is called with the validation result after the validation is performed.
+    ///
+    /// - Returns: A view that can be modified further or used in your SwiftUI hierarchy.
+    func validation<T>(
+        _ item: Binding<T>,
+        rules: [any IValidationRule<T>],
+        action: @escaping (ValidationResult) -> Void
+    ) -> some View {
+        let result = validator.validate(input: item.wrappedValue, rules: rules)
+        action(result)
+        return self
+    }
+}
