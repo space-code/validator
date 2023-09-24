596ae17a605d28d828042b88751f83d88811c772
Nikita Vasilev <nv3212@gmail.com>
2023-09-24 14:26:13 +0400

    Implement `ValidationViewModifier`

---
+++ Sources/ValidatorUI/Classes/Extensions/View+EraseToAnyView.swift
@@ -0,0 +1,12 @@
+//
+// Validator
+// Copyright © 2023 Space Code. All rights reserved.
+//
+
+import SwiftUI
+
+extension View {
+    func eraseToAnyView() -> AnyView {
+        AnyView(self)
+    }
+}
