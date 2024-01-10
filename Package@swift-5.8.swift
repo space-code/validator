864299ff946683cb57b292503337d582eb7b3a76
Nikita Vasilev <nv3212@gmail.com>
2024-01-10 15:16:10 +0100

    Support `visionOS`

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,7 +10,6 @@ let package = Package(
         .macOS(.v13),
         .watchOS(.v9),
         .tvOS(.v16),
-        .visionOS(.v1),
     ],
     products: [
         .library(name: "ValidatorCore", targets: ["ValidatorCore"]),
