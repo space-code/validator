1b02ca2233bb29c16350703135733a45a1d4c16a
Nikita Vasilev <nv3212@gmail.com>
2025-11-23 00:25:53 +0400

    fix: ensure form manager correctly updates isValid

--- Sources/ValidatorUI/Classes/SUI/Managers/FormField/FormFieldManager/FormFieldManager.swift
+++ Sources/ValidatorUI/Classes/SUI/Managers/FormField/FormFieldManager/FormFieldManager.swift
@@ -10,8 +10,9 @@ import ValidatorCore
 public final class FormFieldManager: IFormFieldManager {
     // MARK: Properties
 
-    @Published public var isValid = true
+    @Published public var isValid = false
 
+    private var cancellables = Set<AnyCancellable>()
     private var validators: [any IFormValidationContainer] = []
 
     // MARK: Initialization
@@ -21,6 +22,14 @@ public final class FormFieldManager: IFormFieldManager {
     // MARK: IFormFieldManager
 
     public func append(validator: some IFormValidationContainer) {
+        validator
+            .publisher
+            .sink(receiveValue: { [weak self] _ in
+                self?.validate()
+            }
+            )
+            .store(in: &cancellables)
+
         validators.append(validator)
         validate()
     }
